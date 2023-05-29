d15f373e44e06b0d2e047cc10a064060459b563d
masepe95 <marcosepe10@gmail.com>
2023-05-29 16:57:33 +0200

    Ticket items added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ console.log('JS OK')
 
 // RECUPERO ELEMENTI
 const totalCost = document.getElementById('ticket-cost');
+const userSaved = document.getElementById('saved');
+const userTicketNumber = document.getElementById('ticket-number');
+const userWagon = document.getElementById('wagon');
 
 
 // COSTO PER CHILOMETRO
@@ -25,7 +28,7 @@ button.addEventListener('click', function(){
     const age = (userAge.value);
     // COSTO BIGLIETTO SENZA SCONTI
     const totalPrice = distance * ticketCostKM;
-    console.log(`€ ${totalPrice.toFixed(2)}`);
+    totalCost.innerText = '€' + totalPrice.toFixed(2);
     
     // CALCOLO COSTO BIGLIETTO AL NETTO DELLO SCONTO
     
@@ -33,11 +36,15 @@ button.addEventListener('click', function(){
         let minorNetDiscount = (totalPrice * minorDiscount);
         let minorTotalPrice = totalPrice - minorNetDiscount;
         console.log(`€${minorTotalPrice.toFixed(2)}`);
+        totalCost.innerText = '€' + minorTotalPrice.toFixed(2);
+        saved.innerText = 'Hai Risparmiato €' + minorNetDiscount.toFixed(2);
     }
     else if (age === 'senior' ) {
         let seniorNetDiscount = (totalPrice * seniorDiscount);
         let seniorTotalPrice = totalPrice - seniorNetDiscount;
         console.log(`€${seniorTotalPrice.toFixed(2)}`);
+        totalCost.innerText = '€' + seniorTotalPrice.toFixed(2);
+        saved.innerText = 'Hai Risparmiato €' + seniorNetDiscount.toFixed(2);
     }
     
 })
